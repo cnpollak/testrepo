042163ecd6f64f51b5a5d5195457b3d71b356682
cnpollak <69435166+cnpollak@users.noreply.github.com>
2021-07-02 08:54:26 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 08:53:08 2021
+
+@author: Carol.Pollak
+"""
+
+print("This is a test file for Github.")
+print("edX DS & ML Capstone Project, MOD 1")
